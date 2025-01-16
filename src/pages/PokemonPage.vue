cc47f97a2a18e29c694edef37d47aad00c2a454a
Edlith Vinueza <149545949+EdlithVinueza@users.noreply.github.com>
2025-01-15 19:48:16 -0500

    creacion de pagina y componene

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Adivina el pokemon de la imagen</h1>
+    <PokemonImagen />
+    <PokemonOpciones />
+  
+</template>
+
+<script>
+import PokemonImagen from '@/components/PokemonImagen.vue'
+import PokemonOpciones from '@/components/PokemonOpciones.vue'
+export default {
+    components: {
+        PokemonImagen,
+        PokemonOpciones
+    }
+
+}
+</script>
+
+<style>
+
+</style>
